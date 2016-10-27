677ed6d7adca54c399f3d48c51cec86bad97ea5e
Dmitry <master.waider@gmail.com>
2016-10-28 00:51:15 +0600

    28102016

--- app/src/main/java/mvd/pension/SingleFragmentActivity.java
+++ app/src/main/java/mvd/pension/SingleFragmentActivity.java
@@ -312,9 +312,9 @@ public abstract class SingleFragmentActivity extends AppCompatActivity implement
 		int id = item.getItemId();
 
 		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
+		//if (id == R.id.action_settings) {
+		//	return true;
+		//}
 
 		return super.onOptionsItemSelected(item);
 	}
