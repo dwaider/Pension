5136f20e911283e342373522e916ef7465f3a4f5
Dmitry <master.waider@gmail.com>
2017-11-15 23:56:35 +0500

    для перерасчета

--- app/src/main/java/mvd/pension/FragmentActivity/PCalcPensDataFragment.java
+++ app/src/main/java/mvd/pension/FragmentActivity/PCalcPensDataFragment.java
@@ -48,10 +48,6 @@ public class PCalcPensDataFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public PCalcPensDataFragment() {
-        // Required empty public constructor
-    }
-
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -164,16 +160,6 @@ public class PCalcPensDataFragment extends Fragment {
         }
     }
 
-/*    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }*/
 
     @Override
     public void onDetach() {
